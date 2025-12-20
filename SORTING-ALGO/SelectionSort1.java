9e7144805872bbb5e5858d2f11669b685d7e6a7e
sujit <sujitmore271@gmail.com>
2025-12-20 13:59:00 +0530

    new folder added

---
+++ SORTING-ALGO/SelectionSort1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SelectionSort1 {
+
+    public static void Ssort(int[] num) {
+        int n = num.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            // find index of minimum element
+            for (int j = i + 1; j < n; j++) {
+                if (num[j] < num[min]) {
+                    min = j;
+                }
+            }
+
+            // swap after inner loop
+            int temp = num[min];
+            num[min] = num[i];
+            num[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] num = {2, 3, 1, 5, 4};
+        Ssort(num);
+        System.out.println(Arrays.toString(num));
+    }
+}
