e39adef9ce42e1a221a533078f09f466be4fef9d
sujit <sujitmore271@gmail.com>
2025-12-21 16:41:54 +0530

    second largest added solved using largest and secondlargest in loop with if else

---
+++ arrays/Easy_prob/SecondLarges_Optimal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int secondLargestElement(int[] nums) {
+        int n = nums.length ;
+        if(n < 2) return -1;
+        int largest = -1;
+        int secondLargest = -1;
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] > largest){
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if ( nums[i] < largest && nums[i] > secondLargest){
+                secondLargest = nums[i];
+            }
+        }
+        return secondLargest;
+    }
+}
