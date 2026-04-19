6553a141b2cd966232ce9053d4369985d06f78fd
Sujit More <sujitmore271@gmail.com>
2026-04-19 16:03:54 +0530

    feat: solved LowerBound binary search (code+docs)

---
+++ Binary_Search/LowerBound.java
@@ -0,0 +1,26 @@
+package Binary_Search;
+
+public class LowerBound {
+    public static int lowerBound(int[] nums , int target){
+        int low = 0  , high = nums.length-1;
+        int ans = nums.length;
+        while( low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] >= target){
+                ans = mid ; 
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+
+        }
+        return ans ;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,5,8,15,19};
+        int target = 9 ;
+        int ans = lowerBound(nums, target);
+        System.out.println(ans);
+    }
+}
